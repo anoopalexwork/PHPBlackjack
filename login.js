ab1e1eaa95cfeaa6cf85be38f395d28f04aca9e1
Anoop Alex <anoopalexwork@gmail.com>
2020-12-24 09:52:24 +0530

    BJ files

---
+++ login.js
@@ -0,0 +1,22 @@
+function $(id)
+{
+return document.getElementById(id);
+}
+
+$("n").value ="";
+$("p1").value ="";
+$("p2").value ="";
+
+function checkErrs($buttid)
+{
+    $errs = false;
+    /*if ($('n').value=="") {
+        $('errs').innerHTML+="Please enter a username<br>"; $errs=true; }
+    if ($('n').value=="") {
+        $('errs').innerHTML+="Please enter a password<br>"; $errs=true;}*/
+    //alert($buttid);
+    //if (!$errs) $('gate').submit();
+
+    
+ 
+}
